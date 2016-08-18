13eeb234ad09d8292c774a3751ff4289bf463227
leonardo <leonardo.fermin@gmail.com>
2016-08-18 18:45:17 +0200

    with graph building

--- src/exp_fraenkel.cpp
+++ src/exp_fraenkel.cpp
@@ -17,6 +17,56 @@
 
 
 
+class SLAM_node{
+	public:
+	float distance;
+	std::complex<double> position;
+	vector<int> edges;
+
+};
+
+class SLAM_edge{
+	public:
+	SLAM_node* from;
+	SLAM_node* to;
+	void * pepe;
+};
+	
+class UtilityGraph{
+	std::vector<SLAM_node> Nodes;
+	std::vector<SLAM_edge> Edges;
+	
+	public:
+	
+	int find_point_in_node(geometry_msgs::Point node_position){
+		int index=-1;
+		std::complex<double> query_position(node_position.x, node_position.y);
+
+		for (int i=0; i < Nodes.size();i++){
+			float distance = std::norm( Nodes[i].position - query_position) ;
+			if(distance==0){
+				index=i;
+			}
+		}
+		
+		return index;
+	}
+	
+	void build_graph_from_edges(std::vector<geometry_msgs::Point> edge_markers){
+		
+		for (int i=0; i < edge_markers.size();i+=2){
+			int index_from = find_point_in_node ( edge_markers[i] );
+			int index_to   = find_point_in_node ( edge_markers[i+1] );
+
+		}		
+		
+		
+		
+	}
+	
+};
+
+
 
 class ROS_handler
 {
@@ -285,7 +335,11 @@ class ROS_handler
 			}
 			return first_rect;
 		}
-			
+
+		void edges2Graph(){
+			int a=1;
+			std::pair<int,int> xyz;
+		}
 
 };
 
